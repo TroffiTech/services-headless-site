37b809ffb050b94f99d1de68dc1af5285a326c31
someTrickyCase <snowinboots18@gmail.com>
2025-08-27 04:08:44 +0400

    versel spped insigth and analytycs added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,36 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Troffi Services",
-    description: "Troffi web services for managers",
+	title: "Troffi Services",
+	description: "Troffi web services for managers",
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
-        </html>
-    );
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+				<Analytics />
+				<SpeedInsights />
+			</body>
+		</html>
+	);
 }
